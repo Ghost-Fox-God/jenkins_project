1484a424101a5dfdcc829e18619f2d5278098284
Akshay Patel <arp9975@gmail.com>
2025-03-12 16:15:25 -0300

    changed name to test

--- akshay.py
+++ akshay.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', name="Akshay Patel")
+    return render_template('index.html', name="Akshay")
 
 
 if __name__ == '__main__':
